80ef60831dcceb53c75411a270b3409ca1f60648
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-01-24 02:49:25 +0530

    login functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
 
+import authRoutes from './routes/auth';
 import userRoutes from './routes/users';
 
 import 'dotenv/config';
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 app.listen(7000, () => {
