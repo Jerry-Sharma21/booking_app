34afc48748f9afe5b57859f65f7267692628c176
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-01-23 12:49:37 +0530

    typo fix

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
     </div>
   );
 };
