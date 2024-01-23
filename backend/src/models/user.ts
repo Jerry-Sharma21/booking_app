7a71cdff3f533fe566e699d33aa2358b78609cce
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-01-24 02:21:04 +0530

    token authentication

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 export type UserType = {
@@ -26,6 +27,13 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
 });
 
 const User = mongoose.model<UserType>('User', userSchema);
